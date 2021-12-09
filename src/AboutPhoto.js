33bc4d2bc6088963e0cadc55f8d28f960aa2e002
Andrew Nelson <Realseal177@gmail.com>
2021-12-09 15:10:18 -0800

    styling finished

--- src/AboutPhoto.js
+++ src/AboutPhoto.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import theme from "./theme";
 
-const AboutPhoto = props => {
-    return (
-        <div>
-            <h5>{props.photo.explanation}</h5>
-            <p>Date: {props.photo.date}</p>
-            <p>Copyright: {props.photo.copyright}</p>
-        </div>
-    )
-}
+const StyledAboutPhoto = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  h5 {
+    display: flex;
+    justify-content: center;
+    width: 70%;
+    margin: auto;
+    padding: 2%;
+    color: ${(props) => props.theme.white};
+  }
+  p {
+    color: ${(props) => props.theme.tertiaryColor};
+  }
+`;
+
+const AboutPhoto = (props) => {
+  return (
+    <StyledAboutPhoto theme={theme}>
+      <h5>{props.photo.explanation}</h5>
+      <p>Date: {props.photo.date}</p>
+      <p>Copyright: {props.photo.copyright}</p>
+    </StyledAboutPhoto>
+  );
+};
 
 export default AboutPhoto;
